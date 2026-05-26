68a19cf2f34ab22acb68dd6d4dc8b80e709b43d7
GOLDER303 <szymonstepien@interia.pl>
2026-05-26 21:06:34 +0200

    feat: add game page

--- flutter_bridle_course/lib/main.dart
+++ flutter_bridle_course/lib/main.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import "game.dart";
+
 void main() {
   runApp(const MainApp());
 }
@@ -12,7 +14,68 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Align(alignment: Alignment.centerLeft, child: Text("Hello World!")),
+          title: Align(alignment: Alignment.centerLeft, child: Text("Bridle")),
+        ),
+        body: Column(
+          children: [
+            Center(child: GamePage()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsGeometry.all(8),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          ..._game.guesses.map(
+            (guess) => Row(
+              spacing: 5,
+              children: [
+                ...guess.map((letter) => Tile(letter.char, letter.type)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  final String letter;
+  final HitType hitType;
+
+  const Tile(this.letter, this.hitType, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          .hit => Colors.green,
+          .partial => Colors.yellow,
+          .miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
     );
